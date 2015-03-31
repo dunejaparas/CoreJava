23ecb3eee9fe5d1cca3669ba3b60dccfa4d75f75
epardun <paras.duneja@ericsson.com>
2015-03-31 10:41:53 +0200

    updated log4j properties

--- src/main/java/com/pd/core/log4j/MyLog4jExample.java
+++ src/main/java/com/pd/core/log4j/MyLog4jExample.java
@@ -7,7 +7,7 @@ public class MyLog4jExample {
     private final Logger logger;
 
     public MyLog4jExample() {
-	logger = Logger.getLogger("My component logger");
+	logger = Logger.getLogger(this.getClass().getName());
     }
 
     public static void main(final String args[]) {
@@ -15,8 +15,8 @@ public class MyLog4jExample {
     }
 
     private void start() {
-	logger.debug("PARAS");
-
+	logger.debug("Hello this is an debug message");
+	logger.info("Hello this is an info message");
     }
 
 }

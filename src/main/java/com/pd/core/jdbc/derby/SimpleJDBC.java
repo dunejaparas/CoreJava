b53be41ce85536d61509919fd850dd49f7353cd1
epardun <paras.duneja@ericsson.com>
2015-04-22 13:58:41 +0200

    Added derby DB dependency in pom, and loaded Derby Classes in java class

---
+++ src/main/java/com/pd/core/jdbc/derby/SimpleJDBC.java
@@ -0,0 +1,12 @@
+package com.pd.core.jdbc.derby;
+
+public class SimpleJDBC {
+    public static void main(final String args[]) {
+	try {
+	    Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+	    System.out.println("yippie!");
+	} catch (final ClassNotFoundException e) {
+	    e.printStackTrace();
+	}
+    }
+}
